9c6f4e05afc2f797c103991dc9ab8507c455f728
Peder Nygaard <pederany@stud.ntnu.no>
2022-04-25 14:40:03 +0200

    Created the UnitFactory class

---
+++ src/main/java/no/edu/ntnu/idatt2001/pederany/wargames/UnitFactory.java
@@ -0,0 +1,14 @@
+package no.edu.ntnu.idatt2001.pederany.wargames;
+
+import java.util.ArrayList;
+
+public class UnitFactory{
+
+    public Unit createUnit(){
+
+    }
+
+    public ArrayList<Unit> createdUnits(){
+
+    }
+}
