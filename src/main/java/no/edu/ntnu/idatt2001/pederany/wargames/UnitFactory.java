5ca6cbc7241f00a94e9990534c29deb23b5bfed3
Peder Nygaard <pederany@stud.ntnu.no>
2022-05-01 21:45:07 +0200

    Configured the "create unit" and "create list of units" methods in Unit Factory

--- src/main/java/no/edu/ntnu/idatt2001/pederany/wargames/UnitFactory.java
+++ src/main/java/no/edu/ntnu/idatt2001/pederany/wargames/UnitFactory.java
@@ -2,13 +2,60 @@ package no.edu.ntnu.idatt2001.pederany.wargames;
 
 import java.util.ArrayList;
 
-public class UnitFactory{
+public class UnitFactory {
 
-    public Unit createUnit(){
-
+    /**
+     * Creates a single unit based on unit type, name and health
+     * @param unitType Type of unit to create
+     * @param name Name of the unit
+     * @param health Health of the unit
+     * @return A unit based of the registered parameters
+     */
+    public static Unit createUnit(String unitType, String name, int health) {
+        Unit unit = null;
+        switch (unitType) {
+            case "Infantry Unit" -> unit = new InfantryUnit(name, health);
+            case "Ranged Unit" -> unit = new RangedUnit(name, health);
+            case "Cavalry Unit" -> unit = new CavalryUnit(name, health);
+            case "Commander Unit" -> unit = new CommanderUnit(name, health);
+        }
+        return unit;
     }
 
-    public ArrayList<Unit> createdUnits(){
+    /**
+     * Creates a list of units based on several parameters
+     * @param unitType Type of units
+     * @param name Name of the units
+     * @param health Health of the units
+     * @param n Amount of units
+     * @return A list of units based on various parameters
+     */
+    public static ArrayList<Unit> createListOfUnits(String unitType, String name, int health, int n) {
+        ArrayList<Unit> listOfUnits = new ArrayList<Unit>();
 
+        switch (unitType) {
+            case "Infantry Unit":
+                for(int i = 0; i < n; i++) {
+                    //System.out.println("hei");
+                    listOfUnits.add(new InfantryUnit(name, health));
+                }
+                break;
+            case "Ranged Unit":
+                for(int i = 0; i < n; i++) {
+                    listOfUnits.add(new RangedUnit(name, health));
+                }
+                break;
+            case "Cavalry Unit":
+                for(int i = 0; i < n; i++) {
+                    listOfUnits.add(new CavalryUnit(name, health));
+                }
+                break;
+            case "Commander Unit":
+                for(int i = 0; i < n; i++) {
+                    listOfUnits.add(new CommanderUnit(name, health));
+                }
+                break;
+        }
+        return listOfUnits;
     }
 }
